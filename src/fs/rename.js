b377aff3cda7fa9ce0789f889cc427f8e44fc18b
Aldar <aldarkonov@mail.ru>
2022-11-29 20:21:11 +0300

    feat: finish fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import fsp from 'fs/promises';
+import { readFile } from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url);
+    const _dirname = dirname(_filename);
+    const errorMessage = 'FS operation failed';
+
+    const wrongFileName = 'wrongFilename.txt';
+    const wrongFilePath = path.join(_dirname, 'files', wrongFileName);
+    const properFilename = 'properFilename.md';
+    const properFilePath = path.join(_dirname, 'files', properFilename);
+
+    if (!fs.existsSync(wrongFilePath) || fs.existsSync(properFilePath)) {
+        throw new Error(errorMessage);
+    }
+
+    await fsp.rename(wrongFilePath, properFilePath);
+    console.log('File renamed!');
 };
 
 await rename();
