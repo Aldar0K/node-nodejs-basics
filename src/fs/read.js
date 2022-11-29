0c03df1f05f9accbc506593e35a9b63b24ba800e
Aldar <aldarkonov@mail.ru>
2022-11-29 21:04:53 +0300

    feat: finish fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import fsp from 'fs/promises';
+import { readFile } from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const _filename = fileURLToPath(import.meta.url);
+    const _dirname = dirname(_filename);
+    const errorMessage = 'FS operation failed';
+
+    const fileName = 'fileToRead.txt';
+    const filePath = path.join(_dirname, 'files', fileName);
+
+    if (!fs.existsSync(filePath)) {
+        throw new Error(errorMessage);
+    }
+
+    const fileContent = await fsp.readFile(filePath, 'utf-8');
+    console.log(fileContent);
+    console.log('The file has been read!')
 };
 
 await read();
