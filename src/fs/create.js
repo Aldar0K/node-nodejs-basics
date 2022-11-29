2139a74b33592cb40eed041a6af7bed12d45ca75
Aldar <aldarkonov@mail.ru>
2022-11-29 19:58:49 +0300

    feat: finish fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fsp from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
 const create = async () => {
-    // Write your code here 
+    const fileName = 'fresh.txt';
+    const filePath = path.join(_dirname, '/files', fileName);
+    const content = 'I am fresh and young';
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await fsp.writeFile(filePath, content, { flag: 'wx' });
+        console.log('File created!');
+    } catch (err) {
+        throw new Error(errorMessage);
+    }
 };
 
 await create();
