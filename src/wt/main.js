b52186a306c3eab9047d34a34bf350daeff80e47
Aldar <aldarkonov@mail.ru>
2022-12-05 21:07:12 +0300

    feat: finish wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,55 @@
-const performCalculations = async () => {
-    // Write your code here
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+const START_VALUE = 10;
+
+export const performCalculations = async () => {
+    const _filename = fileURLToPath(import.meta.url);
+    const _dirname = path.dirname(_filename);
+
+    const workerName = 'worker.js';
+    const workerPath = path.join(_dirname, workerName);
+    const cpus = os.cpus();
+    const resultArr = [];
+
+    const runWorker = (workerData) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(workerPath, { workerData });
+    
+            worker.on("message", result => {
+                resolve({
+                    'status': 'resolved',
+                    'data': result
+                });
+            });
+
+            worker.on("error", error => {
+                console.log(error);
+            });
+
+            worker.on("exit", exitCode => {
+                if (exitCode !== 0) {
+                    resolve({
+                        'status': 'error',
+                        'data': null,
+                    });
+                }
+            });
+            
+        });
+    }
+
+    for (let i = 0; i < cpus.length; i++ ) {
+        resultArr[i] = runWorker(START_VALUE + i);
+    }
+
+    const resultArray = (await Promise.allSettled(resultArr)).map((item =>{
+        return item.value;
+    }))
+
+    console.log(resultArray);
 };
 
 await performCalculations();
